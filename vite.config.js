5826c1bd8af56d5dc5d1bb57581b6d03c6d9565b
tsv2013 <tsv2013@noreply.github.com>
2025-04-17 16:41:42 +0300

    Added live example to docs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const noCors = () => {
+  return {
+    name: "no-cors",
+    transformIndexHtml(html) {
+      return html.replace(`crossorigin`, "");
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), noCors()],
+  base: "./",
+  build: {
+    outDir: './docs/example',
+    emptyOutDir: false
+  },
   server: {
     host: '0.0.0.0',
+    cors: false,
+  },
+  preview: {
+    cors: false,
   },
   test: {
     global: true,
