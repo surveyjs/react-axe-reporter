62f7ffde6faa1381f9d05c2f8d4862f1b8b75374
tsv2013 <tsv2013@noreply.github.com>
2025-04-17 10:58:37 +0300

    Renamed styles and refactored markup

--- src/report-summary.tsx
+++ src/report-summary.tsx
@@ -1,52 +1,51 @@
 import React from 'react';
-// import styles from './styles/report-summary.module.css';
-import styles from './styles/report.module.css';
+import styles from './styles/rule-summary.module.css';
 
 export default function ReportSummary({ data = [] }) {
   return (
-    <div className={styles.Container_24901_1291670}>
+    <div className={styles.axe_rule_report}>
         <div className={styles.axe_rule_report_header}>
-        <div className={styles.HeaderCell_1_24901_1291672}>
+        <div className={styles.axe_rule_report_header_cell_1}>
             <div className={styles.axe_rule_report_header_cell_content}>
-            <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>#</span></div>
+                <span className={styles.axe_rule_report_header_cell_title_text}>#</span>
             </div>
         </div>
-        <div className={styles.HeaderCell_2_24901_1291673}>
+        <div className={styles.axe_rule_report_header_cell_2}>
             <div className={styles.axe_rule_report_header_cell_content}>
-            <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>Description</span></div>
+                <span className={styles.axe_rule_report_header_cell_title_text}>Description</span>
             </div>
         </div>
-        <div className={styles.HeaderCell_3_24901_1291674}>
+        <div className={styles.axe_rule_report_header_cell_3}>
             <div className={styles.axe_rule_report_header_cell_content}>
-            <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>WCAG / Rule</span></div>
+                <span className={styles.axe_rule_report_header_cell_title_text}>WCAG / Rule</span>
             </div>
         </div>
-        <div className={styles.HeaderCell_4_24901_1291675}>
+        <div className={styles.axe_rule_report_header_cell_4}>
             <div className={styles.axe_rule_report_header_cell_content}>
-            <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>Issues</span></div>
+                <span className={styles.axe_rule_report_header_cell_title_text}>Issues</span>
             </div>
         </div>
         </div>
         {data.map((rule: { id: "", impact: "", tags: [], description: "", help: "", helpUrl: "", nodes: [] }, index: number) =>
         <div className={styles.axe_rule_report_row}>
-            <div className={styles.Cell_1_24901_1291679}>
+            <div className={styles.axe_rule_report_row_cell_1}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919070}><span className={styles.Text_24893_919075}>{index+1}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>{index+1}</span>
                 </div>
             </div>
-            <div className={styles.TableCellDoc_24901_1291680}>
+            <div className={styles.axe_rule_report_row_cell_2}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919088}><span className={styles.Text_24893_919093}>{rule.help}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_link}>{rule.help}</span>
                 </div>
             </div>
-            <div className={styles.TableCellDoc_24901_1291681}>
+            <div className={styles.axe_rule_report_row_cell_3}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_25515_30866}><span className={styles.Text_25515_30867}>{rule.tags.join(", ")}</span><span className={styles.Text_25515_35133}>{rule.id}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>{rule.tags.join(", ")}</span><span className={styles.axe_rule_report_row_cell_additional}>{rule.id}</span>
                 </div>
             </div>
-            <div className={styles.Cell_4_24901_1291682}>
+            <div className={styles.axe_rule_report_row_cell_4}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919070}><span className={styles.Text_24893_919075}>{rule.nodes.length}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>{rule.nodes.length}</span>
                 </div>
             </div>
         </div>)}
--- src/rule-summary.tsx
+++ src/rule-summary.tsx
@@ -1,44 +1,43 @@
 import React from 'react';
-// import styles from './styles/rule-summary.module.css';
-import styles from './styles/report.module.css';
+import styles from './styles/rule-summary.module.css';
 
 export default function RuleSummary({ nodes = [] }) {
   return (
-    <div className={styles.Container_24901_1291670}>
+    <div className={styles.axe_rule_report}>
         <div className={styles.axe_rule_report_header}>
-            <div className={styles.HeaderCell_1_24901_1291672}>
+            <div className={styles.axe_rule_report_header_cell_1}>
                 <div className={styles.axe_rule_report_header_cell_content}>
-                <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>#</span></div>
+                    <span className={styles.axe_rule_report_header_cell_title_text}>#</span>
                 </div>
             </div>
-            <div className={styles.HeaderCell_2_24901_1291673}>
+            <div className={styles.axe_rule_report_header_cell_2}>
                 <div className={styles.axe_rule_report_header_cell_content}>
-                <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>Description</span></div>
+                    <span className={styles.axe_rule_report_header_cell_title_text}>Description</span>
                 </div>
             </div>
-            <div className={styles.HeaderCell_3_24901_1291674}>
+            <div className={styles.axe_rule_report_header_cell_3}>
                 <div className={styles.axe_rule_report_header_cell_content}>
-                <div className={styles.axe_rule_report_header_cell_title}><span className={styles.axe_rule_report_header_cell_title_text}>WCAG / Rule</span></div>
+                    <span className={styles.axe_rule_report_header_cell_title_text}>WCAG / Rule</span>
                 </div>
             </div>
         </div>
         {nodes.map((node: any, index: number) =>
         <div className={styles.axe_rule_report_row}>
-            <div className={styles.Cell_1_24901_1291679}>
+            <div className={styles.axe_rule_report_row_cell_1}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919070}><span className={styles.Text_24893_919075}>{index+1}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>{index+1}</span>
                 </div>
             </div>
-            <div className={styles.Cell_2_24901_1291680}>
+            <div className={styles.axe_rule_report_row_cell_2}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919070}><span className={styles.Text_24893_919075}>Element location:&#xA0;{node.target[0]}<br/>
-                    Element source:&#xA0;{node.html}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>Element location:&#xA0;{node.target[0]}<br/>
+                    Element source:&#xA0;{node.html}</span>
                 </div>
             </div>
-            <div className={styles.Cell_3_24901_1291681}>
+            <div className={styles.axe_rule_report_row_cell_3}>
                 <div className={styles.axe_rule_report_row_cell_content}>
-                <div className={styles.Content_24893_919070}><span className={styles.Text_24893_919075}>{node.failureSummary}<br/>
-                    Related node:&#xA0;{node.target[0]}</span></div>
+                    <span className={styles.axe_rule_report_row_cell_text}>{node.failureSummary}<br/>
+                    Related node:&#xA0;{node.target[0]}</span>
                 </div>
             </div>
         </div>)}
